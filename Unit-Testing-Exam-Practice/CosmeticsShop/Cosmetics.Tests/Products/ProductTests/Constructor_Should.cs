8efe6b9efc754ad817558278ea5705fc571b42f5
pavelangelov <p_a_v_el@abv.bg>
2016-08-08 14:51:57 +0300

    Added ProductTests

---
+++ Unit-Testing-Exam-Practice/CosmeticsShop/Cosmetics.Tests/Products/ProductTests/Constructor_Should.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using NUnit.Framework;
+using Cosmetics.Products;
+using Cosmetics.Common;
+using Cosmetics.Contracts;
+
+namespace Cosmetics.Tests.Products.ProductTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowNullReferenceException_WhenTheNameIsNull()
+        {
+            // Arrange
+            string productName = null;
+            var brand = "Clear";
+            var price = 2m;
+            var genderType = GenderType.Unisex;
+            var milliliters = 330u;
+            var usage = UsageType.EveryDay;
+
+            // Act, Assert
+            Assert.Throws<NullReferenceException>(() => new Shampoo(productName, brand, price, genderType, milliliters, usage));
+        }
+
+        [Test]
+        [TestCase("aa")]
+        [TestCase("Sport-Sport")]
+        public void ThrowIndexOutOfRangeException_WhenTheNameLengthIsInvalid(string invalidNameLength)
+        {
+            // Arrange
+            var brand = "Clear";
+            var price = 2m;
+            var genderType = GenderType.Unisex;
+            var milliliters = 330u;
+            var usage = UsageType.EveryDay;
+
+            // Act, Assert
+            Assert.Throws<IndexOutOfRangeException>(() => new Shampoo(invalidNameLength, brand, price, genderType, milliliters, usage));
+        }
+
+        [Test]
+        public void ThrowNullReferenceException_WhenTheBranchIsNull()
+        {
+            // Arrange
+            var productName = "Sport";
+            string brand = null;
+            var price = 2m;
+            var genderType = GenderType.Unisex;
+            var milliliters = 330u;
+            var usage = UsageType.EveryDay;
+
+            // Act, Assert
+            Assert.Throws<NullReferenceException>(() => new Shampoo(productName, brand, price, genderType, milliliters, usage));
+        }
+
+        [Test]
+        [TestCase("a")]
+        [TestCase("Clear-Sport")]
+        public void ThrowIndexOutOfRangeException_WhenTheBranchLengthIsInvalid(string invalidBranchLength)
+        {
+            // Arrange
+            var name = "Sport";
+            var price = 2m;
+            var genderType = GenderType.Unisex;
+            var milliliters = 330u;
+            var usage = UsageType.EveryDay;
+
+            // Act, Assert
+            Assert.Throws<IndexOutOfRangeException>(() => new Shampoo(name, invalidBranchLength, price, genderType, milliliters, usage));
+        }
+
+        [Test]
+        public void CreateObjectWithAllProperties_IfPassedParametersAreCorrect()
+        {
+            // Arrange
+            var name = "Sport";
+            var brand = "Clear";
+            var price = 2m;
+            var genderType = GenderType.Unisex;
+            var milliliters = 330u;
+            var usage = UsageType.EveryDay;
+            IProduct product = new Shampoo(name, brand, price, genderType, milliliters, usage);
+
+            // Act, Assert
+            Assert.AreEqual(name, product.Name);
+            Assert.AreEqual(brand, product.Brand);
+            Assert.AreEqual(price * milliliters, product.Price);
+            Assert.AreEqual(genderType, product.Gender);
+
+        }
+    }
+}
--- Unit-Testing-Exam-Practice/CosmeticsShop/Tests Requirements.md
+++ Unit-Testing-Exam-Practice/CosmeticsShop/Tests Requirements.md
@@ -21,6 +21,7 @@
  - Constructor should throw NullReferenceException, when the Name is null.
  - Constructor should throw IndexOutOfRangeException, when the Name.Length is smaller than 3. 
  - Constructor should throw IndexOutOfRangeException, when the Name.Length is greather than 10.
+ - Constructor should throw NullReferenceException, when the Branch is null.
  - Constructor should throw IndexOutOfRangeException, when the Branch.Length is smaller than 2. 
  - Constructor should throw IndexOutOfRangeException, when the Branch.Length is greather than 10.
  - Constructor should create object with all properties, if passed parameters are correct.
