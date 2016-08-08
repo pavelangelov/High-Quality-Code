e0ad90bd5ac8c1ce629c04459e11227b8568f54c
pavelangelov <p_a_v_el@abv.bg>
2016-08-08 13:36:13 +0300

    Added CategoryTests

---
+++ Unit-Testing-Exam-Practice/CosmeticsShop/Cosmetics.Tests/Products/CategoryTests/AddCosmetics_Should.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using NUnit.Framework;
+using Moq;
+
+using Cosmetics.Products;
+using Cosmetics.Contracts;
+
+namespace Cosmetics.Tests.Products.CategoryTests
+{
+    [TestFixture]
+    public class AddCosmetics_Should
+    {
+        [Test]
+        public void ThrowNullReferenceException_WhenPassedParameterIsNull()
+        {
+            // Arrange
+            var categoryName = "Shampoo";
+            var category = new Category(categoryName);
+
+            // Act, Assert
+            Assert.Throws<NullReferenceException>(() => category.AddCosmetics(null));
+        }
+
+        [Test]
+        public void AddProduct_WhenPassedParameterIsCorrect()
+        {
+            // Becouse products is private field, if we add product to collection, the Print() should return
+            // "{categoryName} category - {products.Count} {product/products} in total"
+
+            // Arrange
+            var categoryName = "Shampoo";
+            var expectedResult = "Shampoo category - 1 product in total";
+            var category = new Category(categoryName);
+            var mockedProduct = new Mock<IProduct>();
+
+            // Act
+            category.AddCosmetics(mockedProduct.Object);
+
+            // Assert
+            Assert.AreEqual(expectedResult, category.Print());
+        }
+    }
+}
---
+++ Unit-Testing-Exam-Practice/CosmeticsShop/Cosmetics.Tests/Products/CategoryTests/Constructor_Should.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using Cosmetics.Products;
+
+namespace Cosmetics.Tests.Products.CategoryTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowNullReferenceException_WhenNameIsNull()
+        {
+            // Arrange, Act, Assert
+            Assert.Throws<NullReferenceException>(() => new Category(null));
+        }
+
+        [Test]
+        [TestCase("a")]
+        [TestCase("aaaaaaaaaaaaaaaa")]
+        public void ThrowIndexOutOfRangeException_WhenNameLengthIsInvalid(string nameWithInvalidLength)
+        {
+            // Arrange, Act, Assert
+            Assert.Throws<IndexOutOfRangeException>(() => new Category(nameWithInvalidLength));
+        }
+
+        [Test]
+        [TestCase("aa")]
+        [TestCase("aaaaaaaaaaaaaaa")]
+        public void SetName_IfPassedParameterIsCorrect(string nameWithValidLength)
+        {
+            // Arrange
+            var category = new Category(nameWithValidLength);
+            
+            // Act, Assert
+            Assert.AreEqual(nameWithValidLength, category.Name);
+        }
+    }
+}
