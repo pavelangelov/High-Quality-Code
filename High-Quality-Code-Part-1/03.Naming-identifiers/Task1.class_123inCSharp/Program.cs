5740671eb31c1a22cd0b7cd8bc93f9b2433ff81d
pavelangelov <p_a_v_el@abv.bg>
2016-08-20 21:12:35 +0300

    Added solution for task-1

---
+++ High-Quality-Code-Part-1/03.Naming-identifiers/Task1.class_123inCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task1.class_123inCSharp
+{
+    class Foo
+    {
+        const int MaxCount = 6;
+
+        class Bar
+        {
+            internal void PrintTrueOrFalseToConsole(bool booleanToPrint)
+            {
+                string output = booleanToPrint.ToString();
+                Console.WriteLine(output);
+            }
+        }
+        public static void Метод_За_Вход()
+        {
+            Foo.Bar fooBar = new Foo.Bar();
+            fooBar.PrintTrueOrFalseToConsole(true);
+        }
+    }
+}
