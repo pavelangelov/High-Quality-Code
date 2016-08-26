c9d4d4e0a2c53da59d01dfce8d34041a367cbea0
pavelangelov <p_a_v_el@abv.bg>
2016-08-26 20:52:18 +0300

    Fixed errors with StyleCop

---
+++ High-Quality-Code-Part-1/03.Naming-identifiers/Task1.class_123inCSharp/Bar.cs
@@ -0,0 +1,13 @@
+﻿namespace Task1.Class_123inCSharp
+{
+    using System;
+
+    internal class Bar
+    {
+        internal void PrintTrueOrFalseToConsole(bool booleanToPrint)
+        {
+            string output = booleanToPrint.ToString();
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ High-Quality-Code-Part-1/03.Naming-identifiers/Task1.class_123inCSharp/Foo.cs
@@ -0,0 +1,11 @@
+﻿namespace Task1.Class_123inCSharp
+{
+    public class Foo
+    {
+        public static void Main()
+        {
+            Bar fooBar = new Bar();
+            fooBar.PrintTrueOrFalseToConsole(true);
+        }
+    }
+}
