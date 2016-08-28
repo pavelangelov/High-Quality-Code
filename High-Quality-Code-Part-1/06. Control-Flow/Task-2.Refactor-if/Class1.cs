d803951304940ee37bbe403936bdfac34b44590e
pavelangelov <p_a_v_el@abv.bg>
2016-08-28 14:50:42 +0300

    Init commit

---
+++ High-Quality-Code-Part-1/06. Control-Flow/Task-2.Refactor-if/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_2.Refactor
+{
+    public class Foo
+    {
+        public void Bar()
+        {
+            Potato potato;
+            //... 
+            if (potato != null)
+                if (!potato.HasNotBeenPeeled && !potato.IsRotten)
+                    Cook(potato);
+        }
+
+        public void AnotherBar()
+        {
+            if (x >= MIN_X && (x =< MAX_X && ((MAX_Y >= y && MIN_Y <= y) && !shouldNotVisitCell)))
+            {
+                VisitCell();
+            }
+        }
+    }
+}
