39d05f5078890ec05ae6f872018c3f837a9cf742
pavelangelov <p_a_v_el@abv.bg>
2016-08-28 14:30:10 +0300

    Added homework

---
+++ High-Quality-Code-Part-1/05.Variables/Task 2.Method PrintStatistics/Print.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_2.Method_PrintStatistics
+{
+    using System;
+
+    public static class Print
+    {
+        public static void PrintStatistics(double[] arr, int count)
+        {
+            double maxValue = double.MinValue;
+            double minValue = double.MaxValue;
+            double sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                double currentValue = arr[i];
+                if (currentValue > maxValue)
+                {
+                    maxValue = currentValue;
+                }
+
+                if (currentValue < minValue)
+                {
+                    minValue = currentValue;
+                }
+
+                sum += currentValue;
+            }
+
+            double average = sum / count;
+
+            Console.WriteLine(maxValue);
+            Console.WriteLine(minValue);
+            Console.WriteLine(average);
+        }
+    }
+}
---
+++ High-Quality-Code-Part-1/05.Variables/Task-1.Class Size/Size.cs
@@ -0,0 +1,66 @@
+﻿namespace Task_1.Class_Size
+{
+    using System;
+
+    public class Size
+    {
+        private double width;
+        private double height;
+
+        public Size(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                this.height = value;
+            }
+        }
+
+        /// <summary>
+        /// Return new Size with rotated width and height, by passed angle.
+        /// </summary>
+        /// <param name="size"></param>
+        /// <param name="angle"></param>
+        /// <returns></returns>
+        public static Size GetRotatedSize(Size size, double angle)
+        {
+            var rotatedWidth = GetRotateWidth(size, angle);
+            var rotatedHeight = GetRotateHeight(size, angle);
+
+            return new Size(rotatedWidth, rotatedHeight);
+        }
+
+        private static double GetRotateWidth(Size size, double angle)
+        {
+            return Math.Abs(Math.Cos(angle)) * size.width + Math.Abs(Math.Sin(angle)) * size.height;
+        }
+
+        private static double GetRotateHeight(Size size, double angle)
+        {
+            return Math.Abs(Math.Sin(angle)) * size.width + Math.Abs(Math.Cos(angle)) * size.height;
+        }
+    }
+}
