c0ab4f3fb05d2127725b22e05f9c85d5642bf338
pavelangelov <p_a_v_el@abv.bg>
2016-08-28 14:32:26 +0300

    Fixed Task 2 Title

--- High-Quality-Code-Part-1/05.Variables/Task 2.Method PrintStatistics/Properties/AssemblyInfo.cs
+++ High-Quality-Code-Part-1/05.Variables/Task 2.Method PrintStatistics/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Task 2.Method PrintStatistics")]
+[assembly: AssemblyTitle("Task-2.Method PrintStatistics")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task 2.Method PrintStatistics")]
+[assembly: AssemblyProduct("Task-2.Method PrintStatistics")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
