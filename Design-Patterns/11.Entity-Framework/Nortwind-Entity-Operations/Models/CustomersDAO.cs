4de68995de603cde2f4289202fdf9c835c7b65c0
pavelangelov <p_a_v_el@abv.bg>
2016-10-31 19:05:46 +0200

    Added solution for tasks 1-3.

---
+++ Design-Patterns/11.Entity-Framework/Nortwind-Entity-Operations/Models/CustomersDAO.cs
@@ -0,0 +1,118 @@
+﻿using Nortwind_Entity_Operations.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nortwind_Entity_Operations.Models
+{
+    public static class CustomersDAO
+    {
+        private static NorthwindEntities db = new NorthwindEntities();
+        
+        public static void AddNewCustomer(string companyName, string customerId, string country, string city, string phone)
+        {
+            var logFilePath = @"../../Logs/log.txt";
+            using (var writer = new StreamWriter(logFilePath, true))
+            {
+                db.Database.Log = writer.WriteLine;
+
+                db.Customers.Add(new Customer
+                {
+                    CustomerID = customerId,
+                    CompanyName = companyName,
+                    Country = country,
+                    City = city,
+                    Phone = phone
+                });
+
+                db.SaveChanges();
+            }
+        }
+
+        public static void AddNewCustomer(CustomerPoco customer)
+        {
+            var logFilePath = @"../../Logs/log.txt";
+            using (var writer = new StreamWriter(logFilePath, true))
+            {
+                db.Database.Log = writer.WriteLine;
+
+                db.Customers.Add(new Customer
+                {
+                    CustomerID = customer.CustomerID,
+                    CompanyName = customer.CompanyName,
+                    Country = customer.Country,
+                    ContactName = customer.ContactName,
+                    City = customer.City,
+                    ContactTitle = customer.ContactTitle,
+                    Address = customer.Address,
+                    Region = customer.Region,
+                    PostalCode = customer.PostalCode,
+                    Phone = customer.Phone
+                });
+
+                db.SaveChanges();
+            }
+
+        }
+
+        public static string UpdateCustomer(string customerId, string name)
+        {
+            var customer = db.Customers.Find(customerId);
+
+            if (customer != null)
+            {
+                customer.ContactName = name;
+                db.SaveChanges();
+                return $"Customer with CustomerId: {customerId} updated successefully!";
+            }
+            else
+            {
+                return $"Customer with CustomerId: {customerId} doesn`t exist!";
+            }
+        }
+
+        public static string RemoveCustomer(string customerId)
+        {
+            var customer = db.Customers.Find(customerId);
+            if (customer != null)
+            {
+                db.Customers.Remove(customer);
+                db.SaveChanges();
+                return $"Customer with CustomerId: {customerId} is removed.";
+            }
+            else
+            {
+                return $"Customer with CustomerId: {customerId} doesn`t exist!";
+            }
+
+        }
+
+        public static string ListAllCustomersByOrdersYearAndShippedCountry(int year, string country)
+        {
+            var result = new StringBuilder();
+            var orders = db.Orders.Where(o => o.OrderDate.Value.Year == year && o.ShipCountry == country);
+
+            foreach (var order in orders)
+            {
+                result.AppendLine($"Customer: {order.Customer.CompanyName}, Order date: {order.OrderDate.Value.ToShortDateString()}, Ship country: {order.ShipCountry}");
+            }
+            return result.ToString();
+        }
+
+        public static string ListAllCustomers()
+        {
+            var result = new StringBuilder();
+            foreach (var customer in db.Customers)
+            {
+                result.AppendLine($"CustomerId: {customer.CustomerID}, Contact name: {customer.ContactName}");
+            }
+
+            return result.ToString();
+        }
+        
+    }
+}
---
+++ Design-Patterns/11.Entity-Framework/Nortwind-Entity-Operations/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Nortwind_Entity_Operations.Data;
+using Nortwind_Entity_Operations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nortwind_Entity_Operations
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            // Add new Customer to DB 
+            //CustomersDAO.AddNewCustomer("Telerik", "12347", "Bulgaria", "Sofia", "0888111111");
+
+            // Add new Customer to DB using POCO
+            //CustomersDAO.AddNewCustomer(new CustomerPoco
+            //{
+            //    Address = "Mladost",
+            //    City = "Sofia",
+            //    CompanyName = "Progress",
+            //    ContactName = "Doncho Minkov",
+            //    CustomerID = "DMTAP",
+            //    Country = "Bulgaria",
+            //    Phone = "0888123456"
+            //});
+
+
+            //var allCustomers = CustomersDAO.ListAllCustomers();
+
+            //Console.WriteLine(CustomersDAO.UpdateCustomer("12345", "Pavel Angelov"));
+            //Console.WriteLine(CustomersDAO.UpdateCustomer("12346", "Hristo Hristov"));
+            //Console.WriteLine(CustomersDAO.UpdateCustomer("12347", "Pesho Peshev"));
+
+            //Console.WriteLine(CustomersDAO.ListAllCustomers());
+
+            //var message = CustomersDAO.RemoveCustomer("12347");
+            //Console.WriteLine(message);
+
+            var orders = CustomersDAO.ListAllCustomersByOrdersYearAndShippedCountry(1997, "Canada");
+            Console.WriteLine(orders);
+        }
+    }
+}
